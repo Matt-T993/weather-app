c995222ffac10213c84aea56bf531985cb5e17b0
44912269 <53846145+44912269@users.noreply.github.com>
2022-08-10 15:36:49 +1000

    weather api is working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+import Moment from "moment";
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const formatDate = Moment().format("MMMM Do YYYY");
+
+  // const code = {
+  //   key: "9fdc425d5bbccdd4ec68f66ab6ee47ef",
+  //   base: "https://api.openweathermap.org/data/2.5/",
+  // };
+
+  const url = `${process.env.REACT_APP_BASE}weather?q=${location}&units=metric&appid=${process.env.REACT_APP_KEY}`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={
+        typeof data.main != "undefined"
+          ? data.main.temp > 16
+            ? "app warm"
+            : "app cold"
+          : "app"
+      }
+    >
+      <h1 className="title">Weather App</h1>
+      <div className="search">
+        <input
+          className="search-bar"
+          placeholder="Enter Location"
+          type="text"
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+        />
+      </div>
+      <div className="wrapper">
+        <div className="top">
+          <h1>Today's Weather</h1>
+          <p>{formatDate}</p>
+        </div>
+        <hr />
+        <div className="temperture">
+          <h2>{data.name}</h2>
+          {data.main ? (
+            <h1 className="temp">{data.main.temp.toFixed()}°C</h1>
+          ) : null}
+          {data.weather ? <p>{data.weather[0].main}</p> : null}
+          {data.main ? (
+            <p>Feels like {data.main.feels_like.toFixed()}°C</p>
+          ) : null}
+        </div>
+        <div className="bottom">
+          {/* <div className="description">
+            {data.weather ? <h1>{data.weather[0].description}</h1> : null}
+          </div> */}
+
+          <div className="extra">
+            <div className="humidity">
+              {data.main ? <p>Humidity: {data.main.humidity}%</p> : null}
+            </div>
+            {data.wind ? (
+              <div className="wind">Wind: {data.wind.speed.toFixed()}km/h</div>
+            ) : null}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
